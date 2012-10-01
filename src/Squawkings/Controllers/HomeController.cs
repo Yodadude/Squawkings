53ba02e2f2abc651f78b9d95bab0280a4c31c0db
John Byrne <jbyrne@inlogik.com>
2012-10-01 14:44:52 +1000

    Basic Layout and CSS compelet

--- src/Squawkings/Controllers/HomeController.cs
+++ src/Squawkings/Controllers/HomeController.cs
@@ -13,8 +13,32 @@ namespace Squawkings.Controllers
 
         public ActionResult Index()
         {
-            return View();
+			return View(GetSquawkDisplays());
         }
+
+
+		private static List<SquawkDisplay> GetSquawkDisplays()
+		{
+			var squawks = new List<SquawkDisplay>
+		                      {
+		                          new SquawkDisplay() { Username = "test", FullName = "Test User", Time = DateTime.Now.AddMinutes(0), Content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+		                          new SquawkDisplay() { Username = "test2", FullName = "Test Two", Time = DateTime.Now.AddMinutes(-1), Content = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+		                          new SquawkDisplay() { Username = "test", FullName = "Test User", Time = DateTime.Now.AddMinutes(-2), Content = "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+		                          new SquawkDisplay() { Username = "test2", FullName = "Test Two", Time = DateTime.Now.AddMinutes(-3), Content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+		                          new SquawkDisplay() { Username = "test2", FullName = "Test Two", Time = DateTime.Now.AddMinutes(-4), Content = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+		                          new SquawkDisplay() { Username = "test", FullName = "Test User", Time = DateTime.Now.AddMinutes(-5), Content = "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+		                      };
+			return squawks;
+		}
+
+		public class SquawkDisplay
+		{
+			public string Username { get; set; }
+			public string FullName { get; set; }
+			public string Content { get; set; }
+			public DateTime Time { get; set; }
+			public string AvatarUrl { get; set; }
+		}
 
     }
 }
