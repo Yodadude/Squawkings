fca531015d877fd28e5b366dc1a279402ecfab6e
John Byrne <jbyrne@inlogik.com>
2012-10-08 16:50:07 +1100

    Refactored styling and positioning

--- src/Squawkings/Models/ListSquawkingsViewModel.cs
+++ src/Squawkings/Models/ListSquawkingsViewModel.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Squawkings.Controllers;
 
 namespace Squawkings.Models
 {
 	public class ListSquawkingsViewModel
 	{
 		public string Squawk { get; set; }
-		public List<SquawkDisplay> Squawks { get; set; }
+		public List<HomeController.SquawkDisplay> Squawks { get; set; }
 	}
 }
